9758e6564c349beb97ea8e074b01a84716063b52
john-sengson <j10i_freedom@hotmail.com>
2020-05-25 17:24:24 +1200

    install eslint and update characters migrations

---
+++ migrations/20200525112757_characters.js
@@ -0,0 +1,22 @@
+
+exports.up = (knex) => {
+  return knex.schema.createTable('characters', table => {
+    table.increments('id')
+    table.string('name')
+    table.integer('age')
+    table.string('gender')
+    table.string('race')
+    table.string('job')
+    table.string('height')
+    table.string('weight')
+    table.string('origin')
+    table.string('description')
+    table.string('picture')
+    
+    })
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('characters')
+}
+
